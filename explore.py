eb4f0fedd4706b7f1cf6f8d988ad05abb938a9ab
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-08-24 13:38:30 -0500

    adding presentation

--- explore.py
+++ explore.py
@@ -13,9 +13,27 @@ sys.path.append(home_directory_path +'/utils')
 from prepare_utils import split_data
 
 
+def bin_wins(df):
+    """
+    Bin wins column in a DataFrame into different win groups.
+
+    Parameters:
+        df (DataFrame): Input DataFrame containing a 'W' column.
+
+    Returns:
+        DataFrame: DataFrame with an added 'W_bins' column.
+    """
+    # Define the bin edges and labels
+    bins = [0, 60, 70, 80, 90, 100, float('inf')]
+    labels = ['60-', '60-69', '70-79', '80-89', '90-99', '100+']
+
+    # Create the 'W_bins' column
+    df['W_bins'] = pd.cut(df['W'], bins=bins, labels=labels, right=False)
+    return df
+
+# Read data
 tm_batting = pd.read_csv('data/team_batting.csv')
 tm_pitching = pd.read_csv('data/team_pitching.csv')
-
 
 # Split data
 tm_batting_train, _, _ = split_data(tm_batting,
@@ -25,67 +43,77 @@ tm_batting_train, _, _ = split_data(tm_batting,
 
 tm_pitching_train = tm_pitching.loc[tm_batting_train.index]
 
-
-def bin_wins(df=tm_batting_train):
-    # Define the bin edges and labels
-    bins = [0, 60, 70, 80, 90, 100, float('inf')]
-    labels = ['60-', '60-69', '70-79', '80-89', '90-99', '100+']
-
-    # Create the 'W_bins' column
-    df['W_bins'] = pd.cut(df['W'], bins=bins, labels=labels, right=False)
-    return df
-
+# Apply binning function to both datasets
 tm_batting_train = bin_wins(tm_batting_train)
 tm_pitching_train = bin_wins(tm_pitching_train)
 
 
-def plot_key_corrs(df=tm_batting_train):
-    fig, axes = plt.subplots(1, 2, figsize=(6, 6), gridspec_kw={'wspace': .4, 'width_ratios': [0.8, 1]})
+def plot_key_corrs():
+    """
+    Plot heatmap of key correlations for pitching and batting stats.
 
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
+
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6), gridspec_kw={'wspace': 0.4, 'width_ratios': [0.8, 1]})
+
+    # Stats for pitching correlations
     stats_pitching = ['WHIP', 'OPS', 'OBP', 'SLG', 'FIP', 'TB', 'H9', 'BA', 'H', 'SO/W',
        'BB9', 'BB', 'HardH%', '2B', 'BB%', 'SO%', 'HR9', 'HR', 'SO', 'PAge',
        'EV', 'SO9', 'GB%', 'GB/FB', 'SB', 'GO/AO', 'CS']
 
     sns.heatmap(tm_pitching_train.loc[:, stats_pitching+['W']].corr()['W'].to_frame().iloc[:-1,:],
-                linewidths=.5, annot=True, cmap='RdYlGn', vmin=-1, vmax=1, ax=axes[0], cbar=False)
+                linewidths=0.5, annot=True, cmap='RdYlGn', vmin=-1, vmax=1, ax=axes[0], cbar=False)
 
-    axes[0].set_title('Pitching Stat Correlations' + ' '*10, pad=10)
+    axes[0].set_title('Pitching Stat Correlations' + ' ' * 10, pad=10)
     axes[0].tick_params(axis='both', which='both', bottom=False, left=False)
-        
+
+    # Stats for batting correlations
     stats_batting = ['OPS+', 'TotA', 'rOBA', 'OPS', 'OBP', 'HardH%', 'SecA', 'SLG', 'HR',
      'HR%', 'BA', 'H', '2B', 'BatAge', 'GB/FB', 'Pull%',
      'GB%', 'Oppo%', 'SO%', 'SB%', 'FB%', 'Cent%', 'SO', 'CS', 'SB', 'LD%']
 
     sns.heatmap(tm_batting_train.loc[:, stats_batting+['W']].corr()['W'].to_frame().iloc[:-1,:],
-                linewidths=.5, annot=True, cmap='RdYlGn', vmin=-1, vmax=1, ax=axes[1])
+                linewidths=0.5, annot=True, cmap='RdYlGn', vmin=-1, vmax=1, ax=axes[1])
 
-    axes[1].set_title('Offensive Stat Correlations' + ' '*10, pad=10)
+    axes[1].set_title('Offensive Stat Correlations' + ' ' * 10, pad=10)
     axes[1].tick_params(axis='both', which='both', bottom=False, left=False)
 
     plt.show()
 
+def plot_percent_medians():
+    """
+    Plot bar plots showing median percentages of Pull, Cent, and Oppo for different win groups.
 
-def plot_percent_medians(df=tm_batting_train):
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(9, 2.5))
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
 
-    
-    sns.barplot(data=df, x='W_bins', y='Pull%', errorbar=None,
-                order= sorted(df['W_bins'].unique())[1:] + ['100+'],
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 3))
+
+    # Plot Pull% median
+    sns.barplot(data=tm_batting_train, x='W_bins', y='Pull%', errorbar=None,
+                order=sorted(tm_batting_train['W_bins'].unique())[1:] + ['100+'],
                 palette=['#0955AA'], ax=axes[0], estimator='median')
-    axes[0].set_yticks(np.arange(0,31,10))
+    axes[0].set_yticks(np.arange(0, 31, 10))
 
-    
-    sns.barplot(data=df, x='W_bins', y='Cent%', errorbar=None,
-                order= sorted(df['W_bins'].unique())[1:] + ['100+'],
+    # Plot Cent% median
+    sns.barplot(data=tm_batting_train, x='W_bins', y='Cent%', errorbar=None,
+                order=sorted(tm_batting_train['W_bins'].unique())[1:] + ['100+'],
                 palette=['#0955AA'], ax=axes[1], estimator='median')
-    axes[1].set_yticks(np.arange(0,61,20))
+    axes[1].set_yticks(np.arange(0, 61, 20))
 
-
-    sns.barplot(data=df, x='W_bins', y='Oppo%', errorbar=None,
-                order= sorted(df['W_bins'].unique())[1:] + ['100+'],
+    # Plot Oppo% median
+    sns.barplot(data=tm_batting_train, x='W_bins', y='Oppo%', errorbar=None,
+                order=sorted(tm_batting_train['W_bins'].unique())[1:] + ['100+'],
                 palette=['#0955AA'], ax=axes[2], estimator='median')
 
-    axes[2].set_yticks(np.arange(0, 21,5))
+    axes[2].set_yticks(np.arange(0, 21, 5))
 
     axes[0].patches[-2].set_edgecolor('#C40000')
     axes[0].patches[-1].set_edgecolor('#C40000')
@@ -95,70 +123,82 @@ def plot_percent_medians(df=tm_batting_train):
     plt.suptitle('Winning Teams Slightly Pull the Ball More')
     plt.tight_layout()
     sns.despine()
-    
+
     for ax in axes:
-        # make ticks smaller
+        # Make ticks and labels smaller
         ax.tick_params(axis='both', labelsize=8)
+        ax.set_xlabel('Wins')
+
+        # Annotate bars
         for p in ax.patches:
-            # change x labels
-            ax.set_xlabel('Wins')
-            # annotate bars
             ax.annotate(f'{round(p.get_height(), 1)}', (p.get_x() + p.get_width() / 2, p.get_height()),
-                        ha='center', va='bottom',  fontsize=8)
-    
+                        ha='center', va='bottom', fontsize=8)
+
     plt.show()
     
-def plot_wins_vs_percentages(df=tm_batting_train):
-    # Create the subplots
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(7.5, 2.5))
+def plot_wins_vs_percentages():
+    """
+    Plot scatterplots of Wins vs. Pull%, Cent%, and Oppo%.
 
-    # Scatterplot 1
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
+
+    Returns:
+        None
+    """
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(9, 3))
+
+    # Scatterplot 1: Pull%
     sns.scatterplot(data=tm_batting_train, x='Pull%', y='W', s=8, ax=axes[0])
     axes[0].set_xlabel('Pull%')
-    axes[0].set_yticks(ticks=np.arange(50, 121, 10))
     axes[0].set_ylabel('Wins', rotation=0, labelpad=15)
 
-    rectangle = patches.Rectangle((27, 97), 7, 15, linewidth=.5,
-                                  edgecolor='#C40000', facecolor='none')
-    axes[0].add_patch(rectangle)
-    axes[0].add_patch(rectangle)
+    # Add rectangles
+    rectangle1 = patches.Rectangle((27, 97), 7, 15, linewidth=.5,
+                                   edgecolor='#C40000', facecolor='none')
+    axes[0].add_patch(rectangle1)
 
-
-    # Scatterplot 2
+    # Scatterplot 2: Cent%
     sns.scatterplot(data=tm_batting_train, x='Cent%', y='W', s=8, ax=axes[1])
     axes[1].set_xlabel('Cent%')
-    axes[1].set_yticks(ticks=np.arange(50, 121, 10))
     axes[1].set_ylabel('')
-    rectangle = patches.Rectangle((49, 97), 6.5, 14, linewidth=.5,
-                                  edgecolor='#C40000', facecolor='none')
-    axes[1].add_patch(rectangle)
-    axes[1].add_patch(rectangle)
 
+    # Add rectangles
+    rectangle2 = patches.Rectangle((49, 97), 6.5, 14, linewidth=.5,
+                                   edgecolor='#C40000', facecolor='none')
+    axes[1].add_patch(rectangle2)
 
-    # Scatterplot 3
+    # Scatterplot 3: Oppo%
     sns.scatterplot(data=tm_batting_train, x='Oppo%', y='W', s=8, ax=axes[2])
     axes[2].set_xlabel('Oppo%')
-    axes[2].set_yticks(ticks=np.arange(50, 121, 10))
     axes[2].set_ylabel('')
-    rectangle = patches.Rectangle((15.75, 97), 3.25, 15, linewidth=.5,
-                                  edgecolor='#C40000', facecolor='none')
-    axes[2].add_patch(rectangle)
 
-    # Adjust the spacing between subplots
+    # Add rectangles
+    rectangle3 = patches.Rectangle((15.75, 97), 3.25, 15, linewidth=.5,
+                                   edgecolor='#C40000', facecolor='none')
+    axes[2].add_patch(rectangle3)
+
+    # Adjust layout and labels
     plt.suptitle('Winning Teams\' Pull/Center/Oppo Percentages')
     plt.tight_layout()
     sns.despine()
 
     for ax in axes:
-        # make ticks smaller
         ax.tick_params(axis='both', labelsize=8)
 
-    # Show the plot
     plt.show()
-    
-     
-def plot_W_by_OPS(df=tm_batting_train):
-    sns.lmplot(data=df, x='OPS+', y='W', height=3,
+
+def plot_W_by_OPS():
+    """
+    Plot linear regression of Wins vs. OPS+.
+
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
+
+    Returns:
+        None
+    """
+    sns.lmplot(data=tm_batting_train, x='OPS+', y='W', height=3,
                scatter_kws={'s': 2},
                line_kws={'linewidth': .5})
 
@@ -167,7 +207,6 @@ def plot_W_by_OPS(df=tm_batting_train):
     plt.yticks(ticks=np.arange(50, 111, 10), fontsize=8)
     plt.ylabel('Wins', rotation=0, labelpad=15)
 
-
     plt.title('OPS+ leads to more wins', fontsize=10)
 
     plt.tick_params(axis='both', labelsize=7)
@@ -175,18 +214,26 @@ def plot_W_by_OPS(df=tm_batting_train):
     sns.despine()
     plt.show()
 
+def plot_median_OPS():
+    """
+    Plot bar plot of median OPS+ for different win groups.
 
-def plot_median_OPS(df=tm_batting_train):
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
+
+    Returns:
+        None
+    """
     plt.figure(figsize=(4, 2))
 
-    sns.barplot(data=df, x='W_bins', y='OPS+', errorbar=None,
-                order= sorted(df['W_bins'].unique())[1:] + ['100+'],
+    sns.barplot(data=tm_batting_train, x='W_bins', y='OPS+', errorbar=None,
+                order=sorted(tm_batting_train['W_bins'].unique())[1:] + ['100+'],
                 palette=['#0955AA'], estimator='median')
 
     ax = plt.gca()
     for p in ax.patches:
         ax.annotate(f'{round(p.get_height(), 1)}', (p.get_x() + p.get_width() / 2, p.get_height()),
-                         ha='center', va='bottom')
+                    ha='center', va='bottom')
 
     plt.ylabel('OPS+', rotation=0, labelpad=15)
     plt.xlabel('Wins')
@@ -195,13 +242,21 @@ def plot_median_OPS(df=tm_batting_train):
     plt.title('Median OPS+', fontsize=10, pad=15)
     sns.despine()
     plt.show()
-        
-        
-def plot_OPS_distributions(df=tm_batting_train):
+
+def plot_OPS_distributions():
+    """
+    Plot swarm plot of OPS+ distributions for different win groups.
+
+    Parameters:
+        df (DataFrame): Input DataFrame with relevant stats.
+
+    Returns:
+        None
+    """
     plt.figure(figsize=(4, 2.5))
 
-    sns.swarmplot(data=df, x='W_bins', y='OPS+',
-                  order= sorted(df['W_bins'].unique())[1:] + ['100+'],
+    sns.swarmplot(data=tm_batting_train, x='W_bins', y='OPS+',
+                  order=sorted(tm_batting_train['W_bins'].unique())[1:] + ['100+'],
                   size=2.5, color='#0955AA', legend=False)
 
     plt.xlabel('Wins')
--- model.py
+++ model.py
@@ -20,24 +20,33 @@ from sklearn.preprocessing import MinMaxScaler
 
 def split_mlb_data(tm_batting=pd.read_csv('data/team_batting.csv'), 
                    tm_pitching=pd.read_csv('data/team_pitching.csv')):
+    """
+    Split MLB data into training, validation, and test sets.
+
+    Parameters:
+        tm_batting (DataFrame): Batting data.
+        tm_pitching (DataFrame): Pitching data.
+
+    Returns:
+        train (DataFrame): Merged training data.
+        validate (DataFrame): Merged validation data.
+        test (DataFrame): Merged test data.
+    """
+    # Split batting data
+    tm_batting_train, tm_batting_validate, tm_batting_test = split_data(tm_batting, validate_size=.15, test_size=.15, random_state=123)
     
-    # split batting data
-    tm_batting_train,\
-        tm_batting_validate,\
-            tm_batting_test = split_data(tm_batting, validate_size=.15, test_size=.15, random_state=123)
-    
-    # split pitching data
+    # Split pitching data
     tm_pitching_train = tm_pitching.loc[tm_batting_train.index]
     tm_pitching_validate = tm_pitching.loc[tm_batting_validate.index]
     tm_pitching_test = tm_pitching.loc[tm_batting_test.index]
     
-    # join pitching and batting stats
-    train = pd.merge(left = tm_batting_train, right = tm_pitching_train, on=['year','Tm','W'], 
-         suffixes= ("_bat", "_pit"))
-    validate = pd.merge(left = tm_batting_validate, right = tm_pitching_validate, on=['year','Tm','W'], 
-             suffixes= ("_bat", "_pit"))
-    test = pd.merge(left = tm_batting_test, right = tm_pitching_test, on=['year','Tm','W'], 
-             suffixes= ("_bat", "_pit"))
+    # Join pitching and batting stats
+    train = pd.merge(left=tm_batting_train, right=tm_pitching_train, on=['year', 'Tm', 'W'], 
+                     suffixes=("_bat", "_pit"))
+    validate = pd.merge(left=tm_batting_validate, right=tm_pitching_validate, on=['year', 'Tm', 'W'], 
+                        suffixes=("_bat", "_pit"))
+    test = pd.merge(left=tm_batting_test, right=tm_pitching_test, on=['year', 'Tm', 'W'], 
+                    suffixes=("_bat", "_pit"))
     
     return train, validate, test
 
@@ -47,8 +56,23 @@ def scale_mlb_data(train=split_mlb_data()[0],
                    test=split_mlb_data()[2],
                    features=['OPS+', 'HR_bat', 'BatAge', 'OPS_pit', 'BB_pit', 'PAge'],
                    target=['W']):
-    
-    # separate features to scale
+    """
+    Scale the MLB dataset features and target using Min-Max Scaling.
+
+    Parameters:
+        train (DataFrame): Training data.
+        validate (DataFrame): Validation data.
+        test (DataFrame): Test data.
+        features (list): List of feature columns to scale.
+        target (list): List of target columns.
+
+    Returns:
+        train_sc (DataFrame): Scaled training data.
+        validate_sc (DataFrame): Scaled validation data.
+        test_sc (DataFrame): Scaled test data.
+        scaler (MinMaxScaler): Scaler object used for scaling.
+    """
+    # Separate features to scale
     X_train = train[features]
     X_validate = validate[features]
     X_test = test[features]
@@ -57,7 +81,7 @@ def scale_mlb_data(train=split_mlb_data()[0],
     y_validate = validate[target]
     y_test = test[target]
     
-    # scale data
+    # Scale data
     scaler = MinMaxScaler()
 
     train_sc = pd.concat([pd.DataFrame(data=scaler.fit_transform(X_train),
@@ -97,27 +121,45 @@ def run_baseline_model(train, test, features, target):
     return train_rmse, train_r2, test_rmse, test_r2
 
 def run_linear_model(train, test, features, target, scaler):
-    
-    # split X and y
+    """
+    Run a linear regression model on the given training and test data.
+
+    Parameters:
+        train (DataFrame): Training data.
+        test (DataFrame): Test data.
+        features (list): List of feature columns.
+        target (str): Target column.
+        scaler: Scaler object for feature scaling.
+
+    Returns:
+        train_rmse (float): Root Mean Squared Error (RMSE) on the training data.
+        train_r2 (float): R-squared score on the training data.
+        test_rmse (float): Root Mean Squared Error (RMSE) on the test data.
+        test_r2 (float): R-squared score on the test data.
+    """
+    # Split X and y
     X_train = train[features]
     X_test = test[features]
 
     y_train = train[target]
     y_test = test[target]
     
-    # run model
+    # Run model
     lm = LinearRegression().fit(X_train, y_train)    
     
-    # RMSE
+    # Calculate RMSE
     train_rmse = mean_squared_error(y_train, lm.predict(X_train), squared=False)
     test_rmse = mean_squared_error(y_test, lm.predict(X_test), squared=False)
-    # R2
+    
+    # Calculate R2 score
     train_r2 = r2_score(y_train, lm.predict(X_train))
     test_r2 = r2_score(y_test, lm.predict(X_test))
     
+    # Print metrics
     print(f'Train:\tRMSE = {round(train_rmse, 2)}\tR2 = {round(train_r2, 2)}')
     print(f'Test:\tRMSE = {round(test_rmse, 2)}\tR2 = {round(test_r2, 2)}')
     
+    # Create DataFrames for scaled and unscaled coefficients
     df_scaled = pd.DataFrame(np.append(lm.coef_, lm.intercept_),
                              index=features + ['Intercept'],
                              columns=['Scaled Coeffs'])
@@ -125,43 +167,57 @@ def run_linear_model(train, test, features, target, scaler):
     df_unscaled = pd.DataFrame(np.append(lm.coef_ * scaler.scale_, lm.intercept_),
                                index=features + ['Intercept'],
                                columns=['Unscaled Coeffs'])
+    
     # Concatenate the two DataFrames horizontally
     result_df = pd.concat([df_scaled, df_unscaled], axis=1)
 
     # Display the combined DataFrame
     display(result_df)
     
-    
     return train_rmse, train_r2, test_rmse, test_r2
 
+
 def run_GBRegression_model(train, test, features, target):
-    # split X and y
+    """
+    Run a Gradient Boosting Regression model on the given training and test data.
+
+    Parameters:
+        train (DataFrame): Training data.
+        test (DataFrame): Test data.
+        features (list): List of feature columns.
+        target (str): Target column.
+
+    Returns:
+        train_rmse (float): Root Mean Squared Error (RMSE) on the training data.
+        train_r2 (float): R-squared score on the training data.
+        test_rmse (float): Root Mean Squared Error (RMSE) on the test data.
+        test_r2 (float): R-squared score on the test data.
+    """
+    # Split X and y
     X_train = train[features]
     X_test = test[features]
 
     y_train = train[target]
     y_test = test[target]
     
-    # run model
-#     gbr = GradientBoostingRegressor(random_state=123).fit(X_train, y_train)
-#     gbr = GradientBoostingRegressor(learning_rate=0.03801806702724453, max_depth=6,
-#                                       n_estimators=611, random_state=123,
-#                                       subsample=0.12157377354410015).fit(X_train, y_train)
+    # Run model
     gbr = GradientBoostingRegressor(learning_rate=0.01, max_depth=4, n_estimators=500,
                                     random_state=123, subsample=0.5)\
                                     .fit(X_train, y_train)
     
-    # RMSE
+    # Calculate RMSE
     train_rmse = mean_squared_error(y_train, gbr.predict(X_train), squared=False)
     test_rmse = mean_squared_error(y_test, gbr.predict(X_test), squared=False)
-    # R2
+    
+    # Calculate R2 score
     train_r2 = r2_score(y_train, gbr.predict(X_train))
     test_r2 = r2_score(y_test, gbr.predict(X_test))
     
+    # Print metrics
     print(f'Train:\tRMSE = {round(train_rmse, 2)}\tR2 = {round(train_r2, 2)}')
     print(f'Test:\tRMSE = {round(test_rmse, 2)}\tR2 = {round(test_r2, 2)}')
-     
-#     # Plot feature importance
+    
+    #     # Plot feature importance
 #     feature_importance = gbr.feature_importances_
 #     sorted_idx = np.argsort(feature_importance)
 #     pos = np.arange(sorted_idx.shape[0]) + 0.5
